635cc8649a8f35a1b2a2dc880c474098ffe440d7
cdelriot1121 <carandresdelrio1121@gmail.com>
2025-07-20 19:54:03 -0500

    feat: mis primera impresiones :c

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php
 
-echo "Hola mundo";
+echo "Hola mundo \n";
+echo "\n";
+$name = "!Hola a la nada!!";
+$int = 56;
+$int2 = 10;
+$suma_variables = $int + $int2;
+$boolean_weirdo = true;
+
+
+
+$suma_rara = 48 + "2"; //consola: 50 💀💀
+
+$intento_concatenar = "20" + "20"; // consola: 40 💀💀
+
+// El simbolo mas no puede concatenar cadenas de texto
+
+echo $suma_variables;
+echo "\n";
+echo $suma_rara;
+echo "\n";
+echo $name;
