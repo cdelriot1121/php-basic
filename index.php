f5d32c4a336fc027753a5f0f65bd02a65563b873
cdelriot1121 <carandresdelrio1121@gmail.com>
2025-07-20 20:03:06 -0500

    feat: mezclando htmlphp

--- index.php
+++ index.php
@@ -2,13 +2,11 @@
 
 echo "Hola mundo \n";
 echo "\n";
-$name = "!Hola a la nada!!";
+$name = "Juanqui!!";
 $int = 56;
 $int2 = 10;
 $suma_variables = $int + $int2;
 $boolean_weirdo = true;
-
-
 
 $suma_rara = 48 + "2"; //consola: 50 💀💀
 
@@ -16,8 +14,38 @@ $intento_concatenar = "20" + "20"; // consola: 40 💀💀
 
 // El simbolo mas no puede concatenar cadenas de texto
 
-echo $suma_variables;
 echo "\n";
-echo $suma_rara;
 echo "\n";
 echo $name;
+
+?>
+
+
+
+<h1>
+    <?= $suma_rara;
+    ?> 
+</h1>
+
+<h1>
+    <?= $suma_variables;
+    ?> 
+</h1>
+
+<h1>
+    <?= "Hola " . $name //para concatenar se utiliza el .
+    ?> 
+</h1>
+
+
+<style> 
+    /* estilos que agrego el midu*/
+    :root{
+        color-scheme:  light dark;
+    }
+
+    body {
+        display: grid;
+        place-content: center;
+    }
+</style>
