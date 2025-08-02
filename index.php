82036f5e96027a3ea12a89837da969e5a51aee73
cdelriot1121 <carandresdelrio1121@gmail.com>
2025-08-02 00:00:40 -0500

    changes: little changes

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 //declarando variables
 echo "Hola mundo "; //echo para Imprimir consola php
 echo " <h1> Hola mundo </h1>";
-$name = "Juanqui!!";
+$name = "Juanqui";
 $age = 20;
 $int = 56;
 $int2 = 10;
@@ -13,11 +13,10 @@ $char_numero = "30";
 $suma_variables = $int + $int2 + $char_numero;
 $boolean_weirdo = true;
 
+// El simbolo mas no puede concatenar cadenas de texto
 $suma_rara = 48 + "2"; //consola: 50 💀💀
 
 $intento_concatenar = "20" + "20"; // consola: 40 💀💀
-
-// El simbolo mas no puede concatenar cadenas de texto
 
 
 //para concatenar texto utilizar .
@@ -56,28 +55,25 @@ echo gettype($var_int_bool);
 
 
 <!-- Imprimir los resultados --->
-<h1>
-    <?= $suma_rara;
+<!-- <h1>
+    <?= $suma_rara
     ?> 
 </h1>
 <h1>
-    <?= $suma_variables;
+    <?= $suma_variables
     ?> 
 </h1>
 <h1>
-    <?= $concatenar_num;
+    <?= $concatenar_num
     ?> 
 </h1>
 <h1>
     <?= "Hola " . $name //para concatenar se utiliza el .
     ?> 
-</h1>
+</h1> -->
 
-<p>
-    <?= $tipo_int1;?>
-    <?= $tipo_name;?> 
-    <?= $tipo_intentoConcatenar;?>
-</p>
+
+
 <style> 
     /* estilos que agrego el midu*/
     :root{
